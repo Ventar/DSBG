46b1801211b0417ed2ec38eea02e5316882b8c1c
Michael Rodenbuecher <ventar@outlook.com>
2020-07-06 12:28:08 +0200

    New dices

---
+++ src/main/java/dsbg/BlueDice.java
@@ -0,0 +1,43 @@
+package dsbg;
+
+import java.util.Random;
+
+public class BlueDice {
+
+	public int roll() {
+
+		Random rand = new Random();
+
+		int dicevalue = rand.nextInt(6);
+		dicevalue = dicevalue + 1;
+
+		int numberOfSwords = 0;
+
+		if (dicevalue == 1 || dicevalue == 2) {
+			numberOfSwords = 1;
+		}
+
+		if (dicevalue == 3 || dicevalue == 4 || dicevalue == 5) {
+			numberOfSwords = 2;
+		}
+
+		if (dicevalue == 6) {
+			numberOfSwords = 3;
+		}
+
+		System.out.println("Dice value ::= [" + dicevalue + "], Number of Swords ::= [" + numberOfSwords + "]");
+
+		return numberOfSwords;
+	}
+
+	public int roll(int numberOfRolls) {
+
+		int result = 0; // ist das gleiche wie -> int result;
+
+		for (int i = 0; i < numberOfRolls; i = i + 1) {
+			result = result + roll();
+		}
+
+		return result;
+	}
+}
--- src/main/java/dsbg/DiceMain.java
+++ src/main/java/dsbg/DiceMain.java
@@ -6,13 +6,21 @@ public class DiceMain {
 
 		BlackDice d1 = new BlackDice();
 
-		int xy = d1.roll();
-
-		System.out.println("Der Wert von xy beträgt " + xy);
-
 		int multiplerolls = d1.roll(4);
 
 		System.out.println("Die Werte von multiplerolls betragen " + multiplerolls);
-	}
 
+		BlueDice d2 = new BlueDice();
+
+		multiplerolls = d2.roll(3);
+
+		System.out.println("Die Werte von d2 betragen " + multiplerolls);
+
+		OrangeDice d3 = new OrangeDice();
+
+		multiplerolls = d3.roll(2);
+		
+		System.out.println("Die Werte von d3 betragen " + multiplerolls);
+
+	}
 }
